97326538d5f926eae9569e54a59374a86d107598
Zergunchig22132 <56889544+Zergunchig22132@users.noreply.github.com>
2023-03-19 10:01:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+import requests
+
+max_velocity = 2 #lightyear/day
+fuel = 0
+kpd = 0.8
+power_per_fuel = 11
+temperature = 0
+oxygen = 0
+sin = 0
+reactor_power = 0
+ship_mass = 0
+min_temperature = 0
+max_temperature = 30
+max_oxygen_consuption = 60
+min_SH_population = 8
+ship_start_mass = 192
+token = 'jh2gskwd'
